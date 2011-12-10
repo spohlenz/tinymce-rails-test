0cc549ff8a1eb1e71900d2fef4fc59c19e804972
Sam Pohlenz <sam@sampohlenz.com>
2011-12-10 14:26:33 +1030

    Basic rails app with posts scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TinyMCERails::Application.config.secret_token = '7fd7d49d608aa27ad0cbd0c764c43eb63861e43df7e189aa845cd3bfb45765c6ef6757407769ed185073eb838f110d9839df58fc177204f9ecb47c563f95f02c'
